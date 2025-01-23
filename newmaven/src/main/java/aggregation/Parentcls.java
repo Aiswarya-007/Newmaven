e81f2f48bcc440ada33ecbf823def0c85d487296
Aiswarya-007 <aiswaryajithin25@gmail.com>
2025-01-23 13:46:48 +0530

    initial commit

---
+++ newmaven/src/main/java/aggregation/Parentcls.java
@@ -0,0 +1,14 @@
+package aggregation;
+
+public class Parentcls 
+{
+	String name;
+	int age;
+	
+	public Parentcls(String name,int age)
+	{
+		this.name=name;
+		this.age=age;
+	}
+
+}
---
+++ newmaven/src/main/java/inheritancepgms/HierarchialA.java
@@ -0,0 +1,11 @@
+package inheritancepgms;
+
+public class HierarchialA 
+{
+
+	public void display()
+	{
+		System.out.println("Hai Iam the Parent class A");
+	}
+
+}
